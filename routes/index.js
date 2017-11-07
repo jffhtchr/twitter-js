21d2a769825f1b66b5cabce2b27351fc6f6b5a8d
Sam Chai <sam.chai0501@gmail.com>
2017-11-07 12:35:45 -0500

    static routing for style.css

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
+'use strict';
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
-
+router.use(express.static(path.join(__dirname, '../public')));
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
