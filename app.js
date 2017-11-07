9d4c215d7bfce9cb8ce0c8e9a46ad2b99cbe17e8
Sam Chai <sam.chai0501@gmail.com>
2017-11-07 11:12:53 -0500

    use nunjucks with template

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require( 'express' );
+/* jshint node: true, esversion: 6 */
+
+
+const express = require('express');
 const app = express();
 const PORT = 3000;
+const nunjucks = require('nunjucks');
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
 });
 
@@ -11,8 +15,27 @@ app.use(function (request, response, next) {
     // call `next`, or else your app will be a black hole — receiving requests but never properly responding
     console.log(request.method + ' ' + request.originalUrl);
     next();
-})
+});
 
-app.get("/", (request, response) => {
+app.get('/', (request, response) => {
     response.send('HI THERE!');
-})
+});
+
+var locals = {
+    title: 'An Example',
+    people: [
+        {name: 'Gandalf'},
+        {name: 'Frodo'},
+        {name: 'Hermione'}
+    ]
+};
+
+var PATH_TO_TEMPLATES = 'views';
+nunjucks.configure(PATH_TO_TEMPLATES, {
+    autoescape: true,
+    express: app
+});
+
+app.get( '/people', function( req, res ) {
+    return res.render( 'index.html', locals) ;
+} ) ;
